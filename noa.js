ad58a32560dcae8d81482147a194143094d8a1a8
medusaX <medusax-628@gmail.com>
2021-09-22 22:03:58 -0500

    templates

--- noa.js
+++ noa.js
@@ -1,1 +1,14 @@
-alert("Hola :D");
+class user
+{
+    contructor(n,c,p,a)
+    {
+        this.nombre = n;
+        this.correo = c;
+        this.pasword = p;
+        this.age = a;
+    }
+    manage()
+    {
+        
+    }
+}
